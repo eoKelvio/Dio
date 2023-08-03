be1cc0c4c93693964d0827766d8f5a26ae20dfa4
Kelvin Silvestre <kelvinsc7@gmail.com>
2023-08-03 20:27:38 -0300

    Implementações nos arquivos python

--- FormacaoPython/Trabalhos/testes.py
+++ FormacaoPython/Trabalhos/testes.py
@@ -1,3 +1,10 @@
-deposito = "Depósito"
+def contar_vogais(texto):
+    vogais= "aeiouAEIOU"
+    contador = 0
+    for letra in texto:
+        if letra in vogais:
+            contador += 1
+    return contador
 
-print(deposito.center(30),"-")
+frase = "Ola, tudo bem?"
+print (contar_vogais(frase))
